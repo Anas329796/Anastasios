openclaw/openclaw

feat(sessions): add adaptive reset mode

src/auto-reply/reply/session.test.ts
@@ -1328,6 +1328,36 @@ describe("initSessionState reset policy", () => {
     expect(result.sessionId).not.toBe(existingSessionId);
   });
 
+  it("keeps sessions in adaptive mode until both rollover conditions are true", async () => {
+    vi.setSystemTime(new Date(2026, 0, 18, 4, 15, 0));
+    const root = await makeCaseDir("openclaw-reset-adaptive-fresh-");
+    const storePath = path.join(root, "sessions.json");
+    const sessionKey = "agent:main:whatsapp:dm:adaptive-fresh";
+    const existingSessionId = "adaptive-fresh-session-id";
+
+    await writeSessionStoreFast(storePath, {
+      [sessionKey]: {
+        sessionId: existingSessionId,
+        updatedAt: new Date(2026, 0, 18, 3, 30, 0).getTime(),
+      },
+    });
+
+    const cfg = {
+      session: {
+        store: storePath,
+        reset: { mode: "adaptive", atHour: 4, idleMinutes: 60 },
+      },
+    } as OpenClawConfig;
+    const result = await initSessionState({
+      ctx: { Body: "hello", SessionKey: sessionKey },
+      cfg,
+      commandAuthorized: true,
+    });
+
+    expect(result.isNewSession).toBe(false);
+    expect(result.sessionId).toBe(existingSessionId);
+  });
+
   it("keeps the existing stale session for /reset soft", async () => {
     vi.setSystemTime(new Date(2026, 0, 18, 5, 30, 0));
     const root = await makeCaseDir("openclaw-reset-soft-stale-");

Anas329796/Anastasios

src/auto-reply/reply/session.test.ts
@@ -1522,6 +1522,38 @@ describe("initSessionState reset policy", () => {
     expect(peekSystemEvents(existingSessionId)).toStrictEqual([]);
   });
 
+  it("keeps sessions in adaptive mode until both rollover conditions are true", async () => {
+    vi.setSystemTime(new Date(2026, 0, 18, 4, 15, 0));
+    const root = await makeCaseDir("openclaw-reset-adaptive-fresh-");
+    const storePath = path.join(root, "sessions.json");
+    const sessionKey = "agent:main:whatsapp:dm:adaptive-fresh";
+    const existingSessionId = "adaptive-fresh-session-id";
+
+    await writeSessionStoreFast(storePath, {
+      [sessionKey]: {
+        sessionId: existingSessionId,
+        sessionStartedAt: new Date(2026, 0, 18, 3, 30, 0).getTime(),
+        lastInteractionAt: new Date(2026, 0, 18, 3, 30, 0).getTime(),
+        updatedAt: new Date(2026, 0, 18, 4, 10, 0).getTime(),
+      },
+    });
+
+    const cfg = {
+      session: {
+        store: storePath,
+        reset: { mode: "adaptive", atHour: 4, idleMinutes: 60 },
+      },
+    } as OpenClawConfig;
+    const result = await initSessionState({
+      ctx: { Body: "hello", SessionKey: sessionKey },
+      cfg,
+      commandAuthorized: true,
+    });
+
+    expect(result.isNewSession).toBe(false);
+    expect(result.sessionId).toBe(existingSessionId);
+  });
+
   it("keeps the existing stale session for /reset soft", async () => {
     vi.setSystemTime(new Date(2026, 0, 18, 5, 30, 0));
     const root = await makeCaseDir("openclaw-reset-soft-stale-");