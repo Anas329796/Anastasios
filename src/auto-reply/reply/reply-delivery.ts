coppolek/openclaw

fix: surface dropped media to users instead of silently swallowing
- Upgrade logVerbose to log.warn in reply-media-paths and agent-runner-payloads
- Add DroppedMediaItem type with sanitized displayName and reason code
- Collect dropped media during normalization in reply-media-paths catch block
- Generate droppedMedia from original media list in agent-runner-payloads catastrophic catch
- Prevent skip of payloads that only carry droppedMedia (reply-delivery, payloads, deliver)
- Append user-visible warning notice to text before sending
- Include dropped media notice in mirrored transcript
- Add test coverage for all dropped media scenarios

src/auto-reply/reply/reply-delivery.ts
@@ -87,7 +87,8 @@ export function createBlockReplyDeliveryHandler(params: {
 }): (payload: ReplyPayload) => Promise<void> {
   return async (payload) => {
     const { text, skip } = params.normalizeStreamingText(payload);
-    if (skip && !resolveSendableOutboundReplyParts(payload).hasMedia) {
+    const hasDroppedMedia = (payload.droppedMedia?.length ?? 0) > 0;
+    if (skip && !resolveSendableOutboundReplyParts(payload).hasMedia && !hasDroppedMedia) {
       return;
     }
 
@@ -125,11 +126,17 @@ export function createBlockReplyDeliveryHandler(params: {
     );
     const blockHasMedia = resolveSendableOutboundReplyParts(blockPayload).hasMedia;
 
-    // Skip empty payloads unless they have audioAsVoice flag (need to track it).
-    if (!blockPayload.text && !blockHasMedia && !blockPayload.audioAsVoice) {
+    const blockHasDroppedMedia = (blockPayload.droppedMedia?.length ?? 0) > 0;
+    // Skip empty payloads unless they have audioAsVoice flag or dropped media notice.
+    if (
+      !blockPayload.text &&
+      !blockHasMedia &&
+      !blockPayload.audioAsVoice &&
+      !blockHasDroppedMedia
+    ) {
       return;
     }
-    if (normalized.isSilent && !blockHasMedia) {
+    if (normalized.isSilent && !blockHasMedia && !blockHasDroppedMedia) {
       return;
     }
 

Anas329796/Anastasios

src/auto-reply/reply/reply-delivery.ts
@@ -86,7 +86,8 @@ export function createBlockReplyDeliveryHandler(params: {
 }): (payload: ReplyPayload) => Promise<void> {
   return async (payload) => {
     const { text, skip } = params.normalizeStreamingText(payload);
-    if (skip && !hasOutboundReplyContent({ ...payload, text: undefined })) {
+    const hasDroppedMedia = (payload.droppedMedia?.length ?? 0) > 0;
+    if (skip && !hasOutboundReplyContent({ ...payload, text: undefined }) && !hasDroppedMedia) {
       return;
     }
 
@@ -131,11 +132,12 @@ export function createBlockReplyDeliveryHandler(params: {
     );
     const blockHasNonTextContent = hasOutboundReplyContent({ ...blockPayload, text: undefined });
 
-    // Skip empty payloads unless they have audioAsVoice flag (need to track it).
-    if (!blockPayload.text && !blockHasNonTextContent && !blockPayload.audioAsVoice) {
+    const blockHasDroppedMedia = (blockPayload.droppedMedia?.length ?? 0) > 0;
+    // Skip empty payloads unless they have audioAsVoice flag or dropped media notice.
+    if (!blockPayload.text && !blockHasNonTextContent && !blockPayload.audioAsVoice && !blockHasDroppedMedia) {
       return;
     }
-    if (normalized.isSilent && !blockHasNonTextContent) {
+    if (normalized.isSilent && !blockHasNonTextContent && !blockHasDroppedMedia) {
       return;
     }
 